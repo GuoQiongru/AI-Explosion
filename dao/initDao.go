eb9bf050b0883035a7d21a4441aa9385b759d54b
misstruth <116658162+misstruth@users.noreply.github.com>
2023-08-14 14:17:15 +0800

    Update initDao.go

--- dao/initDao.go
+++ dao/initDao.go
@@ -1,32 +1,36 @@
 package dao
 
 import (
-  "gorm.io/gorm"
-  "gorm.io/driver/mysql"
-  "gorm.io/gorm/logger"
-  "log"
-  "os"
-  "time"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var Db *gorm.DB
 
 func Init() {
-  newLogger := logger.New(
-    log.New(os.Stdout, "\r\n", log.LstdFlags),
-    logger.Config{
-      SlowThreshold: time.Second,
-      LogLevel:      logger.Error,
-      Colorful:      true,
-    },
-  )
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Error,
+			Colorful:      true,
+		},
+	)
 
-  var err error
-  dsn := "root:root@(127.0.0.1:3306)/douyin?charset=utf8mb4&parseTime=True&loc=Local"
-  Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
-    Logger: newLogger,
-  })
-  if err != nil {
-    log.Panicln("err:", err.Error())
-  }
+	var err error
+	dsn := "root:123456@(47.113.148.197:3306)/douyin?charset=utf8mb4&parseTime=True&loc=Local"
+	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+
+	Db.AutoMigrate(&TableUser{})
+
+	if err != nil {
+		log.Panicln("err:", err.Error())
+	}
 }
